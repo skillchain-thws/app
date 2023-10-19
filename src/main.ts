16bfbcc7e11209ccdc827c865798627ad7a4c228
phatnguyen <phatnguyen@gmx.de>
2023-10-19 18:41:22 +0200

    cp

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,9 @@
+import routes from 'virtual:generated-pages'
 import { createApp } from 'vue'
 import { createRouter, createWebHistory } from 'vue-router'
-import routes from 'virtual:generated-pages'
 import App from './App.vue'
 
 import '@unocss/reset/tailwind.css'
-import './styles/main.css'
 import 'uno.css'
 
 const app = createApp(App)
