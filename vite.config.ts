f95d958b2342bfa02ec5d6a0135aa6ba85522f72
phatnguyen <phatnguyen@gmx.de>
2023-10-19 18:49:15 +0200

    cp

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,10 @@
-/// <reference types="vitest" />
-
 import path from 'node:path'
-import { defineConfig } from 'vite'
-import Vue from '@vitejs/plugin-vue'
-import Pages from 'vite-plugin-pages'
-import Components from 'unplugin-vue-components/vite'
-import AutoImport from 'unplugin-auto-import/vite'
+import vue from '@vitejs/plugin-vue'
 import UnoCSS from 'unocss/vite'
-import VueMacros from 'unplugin-vue-macros/vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { defineConfig } from 'vite'
+import Pages from 'vite-plugin-pages'
 
 export default defineConfig({
   resolve: {
@@ -16,23 +13,15 @@ export default defineConfig({
     },
   },
   plugins: [
-    VueMacros({
-      defineOptions: false,
-      defineModels: false,
-      plugins: {
-        vue: Vue({
-          script: {
-            propsDestructure: true,
-            defineModel: true,
-          },
-        }),
+    vue({
+      script: {
+        propsDestructure: true,
+        defineModel: true,
       },
     }),
 
-    // https://github.com/hannoeru/vite-plugin-pages
     Pages(),
 
-    // https://github.com/antfu/unplugin-auto-import
     AutoImport({
       imports: [
         'vue',
@@ -46,18 +35,11 @@ export default defineConfig({
       vueTemplate: true,
     }),
 
-    // https://github.com/antfu/vite-plugin-components
     Components({
       dts: true,
     }),
 
-    // https://github.com/antfu/unocss
-    // see uno.config.ts for config
     UnoCSS(),
   ],
 
-  // https://github.com/vitest-dev/vitest
-  test: {
-    environment: 'jsdom',
-  },
 })
