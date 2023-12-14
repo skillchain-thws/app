7919a20fe4a74740044b9c5c9c4bf19a04753229
phatnguyen <phatnguyen@gmx.de>
2023-12-14 19:13:23 +0100

    fe: build

---
+++ frontend/dist/assets/ProfileCard.vue_vue_type_script_setup_true_lang-rltqEKWD.js
@@ -0,0 +1,11 @@
+import{_ as y,h as v,a as w,b as B,c as $,i as V,d as x,e as N}from"./Label.vue_vue_type_script_setup_true_lang-vZWXejyo.js";import{D as d,d as z,o as n,b as r,w as t,t as a,f as e,u as _,e as s,F as i,m as D,A as F,c as l,p as I}from"./index-hpl1SOig.js";/**
+ * @license lucide-vue-next v0.294.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const S=d("BookmarkIcon",[["path",{d:"m19 21-7-4-7 4V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2v16z",key:"1fy3hk"}]]);/**
+ * @license lucide-vue-next v0.294.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const E=d("StarIcon",[["polygon",{points:"12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2",key:"8f66p6"}]]),L={class:"flex justify-between"},T={class:"break-words"},j={class:"break-words"},A={class:"flex items-center gap-2"},H={class:"font-bold text-lg"},P={class:"text-muted-foreground"},q={class:"mt-2 flex items-center"},G={class:"font-bold mr-0.5"},Q=z({__name:"ProfileCard",props:{title:{},badges:{},description:{},budget:{},star:{},ratingCount:{}},setup(J){return(o,K)=>{const m=y,p=v,u=w,f=B,h=$,g=V,k=x,C=N;return n(),r(C,{class:"group-hover:border-primary transition-colors"},{default:t(()=>[s(f,null,{default:t(()=>[s(m,null,{default:t(()=>[e("div",L,[e("span",T,a(o.title),1),s(_(S),{size:22,class:"hover:fill-white"})])]),_:1}),s(u,{class:"pt-2 space-x-2"},{default:t(()=>[(n(!0),l(i,null,I(o.badges,(c,b)=>(n(),l(i,null,[c?(n(),r(p,{key:b,variant:"secondary"},{default:t(()=>[D(a(c),1)]),_:2},1024)):F("",!0)],64))),256))]),_:1})]),_:1}),s(h,null,{default:t(()=>[e("div",j,a(o.description),1)]),_:1}),s(k,null,{default:t(()=>[e("div",A,[s(_(E),{class:"fill-white -mt-0.5",size:20}),e("span",H,a(o.star),1),e("span",P,"("+a(o.ratingCount)+")",1)]),e("div",q,[e("span",G," eth "+a(o.budget),1),s(g)])]),_:1})]),_:1})}}});export{Q as _};
