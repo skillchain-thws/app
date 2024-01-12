a90c64ba871d231c5b8456dfcbef358170a3b928
phatnguyen <phatnguyen@gmx.de>
2024-01-12 01:05:48 +0100

    fe: build

--- assets/Label.vue_vue_type_script_setup_true_lang-7dWBoaux.js
+++
@@ -1,1 +0,0 @@
-import{d as t,o as r,a as n,w as l,r as c,m as p,u as e,g as f,ak as d}from"./index-CnjvNhTO.js";const _=t({__name:"Label",props:{for:{},asChild:{type:Boolean},as:{},class:{}},setup(a){const s=a;return(o,m)=>(r(),n(e(d),p(s,{class:e(f)("block font-medium text-foreground text-left",s.class)}),{default:l(()=>[c(o.$slots,"default")]),_:3},16,["class"]))}});export{_};
