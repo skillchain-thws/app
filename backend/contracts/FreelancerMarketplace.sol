7c18d67e36a3c45987bf81c8a903e8558c6754f9
sobs1908 <sebastian.hofstetter@student.fhws.de>
2024-01-14 03:10:43 +0100

    Added ReviewManager.sol and ChatManager.sol Added function isEscrowEntity() in EscrowManager.sol Removed existing Review struct in Freelancermarketplace.sol

--- backend/contracts/FreelancerMarketplace.sol
+++ backend/contracts/FreelancerMarketplace.sol
@@ -4,31 +4,25 @@ import "./UserManager.sol";
 import "./JobManager.sol";
 import "./EscrowManager.sol";
 
-struct Review {
-  string comment;
-  uint8 rating;
-  address commenter;
-}
-
 contract FreelancerMarketplace {
-  address owner;
-  UserManager userManager;
-  JobManager jobManager;
-  EscrowManager escrowManager;
+    address owner;
+    UserManager userManager;
+    JobManager jobManager;
+    EscrowManager escrowManager;
 
-  //*********************************************************************
-  //*********************************************************************
-  //                        Utility Functions
-  //*********************************************************************
-  //*********************************************************************
+    //*********************************************************************
+    //*********************************************************************
+    //                        Utility Functions
+    //*********************************************************************
+    //*********************************************************************
 
-  function onlyAdmin() external view returns (bool) {
-    require(msg.sender != owner, "You need to be a Admin for this action");
-    return true;
-  }
+    function onlyAdmin() external view returns (bool) {
+        require(msg.sender != owner, "You need to be a Admin for this action");
+        return true;
+    }
 
-  function nonEmptyString(string memory str) external pure returns (bool) {
-    require(bytes(str).length > 0, "String must not be empty");
-    return true;
-  }
+    function nonEmptyString(string memory str) external pure returns (bool) {
+        require(bytes(str).length > 0, "String must not be empty");
+        return true;
+    }
 }
