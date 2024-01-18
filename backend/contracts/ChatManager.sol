973e85ac539ebca215e14f8a77643017df76c572
sobs1908 <sebastian.hofstetter@student.fhws.de>
2024-01-18 19:27:55 +0100

    Added  CommitteeManager.sol - Temporary removed usage usage of Modifier in ChatManager.sol - Added CommitteManager.sol - Adjusted UserManager to match committee logic - Adjusted EscrowManager to match committee logic - Added initial openChannel and initial message after escrow is created

--- backend/contracts/ChatManager.sol
+++ backend/contracts/ChatManager.sol
@@ -7,203 +7,197 @@ import "./UserManager.sol";
 import "./JobManager.sol";
 
 contract ChatManager {
-    enum ChannelStatus {
-        Open,
-        Closed
-    }
-    EscrowManager escrowManager;
-    FreelancerMarketplace freelancerMarketplace;
-    JobManager jobManager;
-    UserManager userManager;
-    uint channelCount;
+  enum ChannelStatus {
+    Open,
+    Closed
+  }
+  EscrowManager escrowManager;
+  FreelancerMarketplace freelancerMarketplace;
+  JobManager jobManager;
+  UserManager userManager;
+  uint channelCount;
 
-    struct Channel {
-        uint escrowId;
-        address participantSeller;
-        address participantBuyer;
-        ChannelStatus channelStatus;
-        uint messageCount;
-    }
+  struct Channel {
+    uint escrowId;
+    address participantSeller;
+    address participantBuyer;
+    ChannelStatus channelStatus;
+    uint messageCount;
+  }
 
-    struct Message {
-        address sender;
-        address receiver;
-        uint timestamp;
-        string content;
-    }
+  struct Message {
+    address sender;
+    address receiver;
+    uint timestamp;
+    string content;
+  }
 
-    // Mappings to store channels and messages
-    mapping(uint => Channel) public channels;
-    mapping(uint => mapping(uint256 => Message)) private channelMessages;
+  // Mappings to store channels and messages
+  mapping(uint => Channel) public channels;
+  mapping(uint => mapping(uint256 => Message)) private channelMessages;
 
-    constructor(address _freelancerMarketplaceAddress) {
-        freelancerMarketplace = FreelancerMarketplace(
-            _freelancerMarketplaceAddress
-        );
-        channelCount = 0;
-    }
+  constructor(address _freelancerMarketplaceAddress) {
+    freelancerMarketplace = FreelancerMarketplace(
+      _freelancerMarketplaceAddress
+    );
+    channelCount = 0;
+  }
 
-    //*********************************************************************
-    //*********************************************************************
-    //                        Setter Functions
-    //*********************************************************************
-    //*********************************************************************
+  //*********************************************************************
+  //*********************************************************************
+  //                        Setter Functions
+  //*********************************************************************
+  //*********************************************************************
 
-    function setEscrowManager(address _address) external {
-        require(
-            freelancerMarketplace.onlyAdmin(),
-            "Only the Admin can add Managers"
-        );
-        escrowManager = EscrowManager(_address);
-    }
+  function setEscrowManager(address _address) external {
+    require(
+      freelancerMarketplace.onlyAdmin(),
+      "Only the Admin can add Managers"
+    );
+    escrowManager = EscrowManager(_address);
+  }
 
-    function setJobManager(address _address) external {
-        require(
-            freelancerMarketplace.onlyAdmin(),
-            "Only the Admin can add Managers"
-        );
-        jobManager = JobManager(_address);
-    }
+  function setJobManager(address _address) external {
+    require(
+      freelancerMarketplace.onlyAdmin(),
+      "Only the Admin can add Managers"
+    );
+    jobManager = JobManager(_address);
+  }
 
-    function setUserManager(address _address) external {
-        require(
-            freelancerMarketplace.onlyAdmin(),
-            "Only the Admin can add Managers"
-        );
-        userManager = UserManager(_address);
-    }
+  function setUserManager(address _address) external {
+    require(
+      freelancerMarketplace.onlyAdmin(),
+      "Only the Admin can add Managers"
+    );
+    userManager = UserManager(_address);
+  }
 
-    //*********************************************************************
-    //*********************************************************************
-    //                        Getter Functions
-    //*********************************************************************
-    //*********************************************************************
+  //*********************************************************************
+  //*********************************************************************
+  //                        Getter Functions
+  //*********************************************************************
+  //*********************************************************************
 
-    // Retrieve details of a specific channel
-    function getChannel(uint _escrowId) public view returns (Channel memory) {
-        return channels[_escrowId];
-    }
+  // Retrieve details of a specific channel
+  function getChannel(uint _escrowId) public view returns (Channel memory) {
+    return channels[_escrowId];
+  }
 
-    // Retrieve details of a specific message in a channel
-    function getMessage(
-        uint _escrowId,
-        uint _messageId
-    ) public view returns (Message memory) {
-        // Get channel by escrowId
-        Channel storage channel = channels[_escrowId];
+  // Retrieve details of a specific message in a channel
+  function getMessage(
+    uint _escrowId,
+    uint _messageId
+  ) public view returns (Message memory) {
+    // Get channel by escrowId
+    Channel storage channel = channels[_escrowId];
 
-        // Ensure that the messageId is within a valid range
-        require(_messageId < channel.messageCount, "Invalid messageId");
+    // Ensure that the messageId is within a valid range
+    require(_messageId < channel.messageCount, "Invalid messageId");
 
-        return channelMessages[_escrowId][_messageId];
-    }
+    return channelMessages[_escrowId][_messageId];
+  }
 
-    // Retrieve all messages in a channel
-    function getAllChannelMessages(
-        uint _escrowId
-    ) public view returns (Message[] memory) {
-        uint count = channels[_escrowId].messageCount;
-        Message[] memory allMessages = new Message[](count);
+  // Retrieve all messages in a channel
+  function getAllChannelMessages(
+    uint _escrowId
+  ) public view returns (Message[] memory) {
+    uint count = channels[_escrowId].messageCount;
+    Message[] memory allMessages = new Message[](count);
 
-        for (uint i = 0; i < count; i++) {
-            allMessages[i] = channelMessages[_escrowId][i];
-        }
-
-        return allMessages;
+    for (uint i = 0; i < count; i++) {
+      allMessages[i] = channelMessages[_escrowId][i];
     }
 
-    //*********************************************************************
-    //*********************************************************************
-    //                        Channel Functions
-    //*********************************************************************
-    //*********************************************************************
+    return allMessages;
+  }
 
-    // Open a new communication channel for a given escrow
-    function openChannel(uint _escrowId) public isEscrowParty(_escrowId) {
-        // Destruct escrow from getEscrow from EscrowManager to get the addresses of buyer and seller
-        (, , address buyer, address seller, , , , ) = escrowManager.getEscrow(
-            _escrowId
-        );
+  //*********************************************************************
+  //*********************************************************************
+  //                        Channel Functions
+  //*********************************************************************
+  //*********************************************************************
 
-        // Create a new Channel instance and store it in the mapping array
-        channels[_escrowId] = Channel({
-            escrowId: _escrowId,
-            participantSeller: seller,
-            participantBuyer: buyer,
-            channelStatus: ChannelStatus.Open,
-            messageCount: 0
-        });
+  // Open a new communication channel for a given escrow
+  function openChannel(uint _escrowId) public {
+    // Destruct escrow from getEscrow from EscrowManager to get the addresses of buyer and seller
+    (, , address buyer, address seller, , , , ) = escrowManager.getEscrow(
+      _escrowId
+    );
 
-        channelCount++;
-    }
+    // Create a new Channel instance and store it in the mapping array
+    channels[_escrowId] = Channel({
+      escrowId: _escrowId,
+      participantSeller: seller,
+      participantBuyer: buyer,
+      channelStatus: ChannelStatus.Open,
+      messageCount: 0
+    });
 
-    // Close an existing communication channel for a given escrow
-    function closeChannel(uint _escrowId) public isEscrowParty(_escrowId) {
-        // Fetch the corresponding channel
-        Channel storage channel = channels[_escrowId];
+    channelCount++;
+  }
 
-        // Ensure that the channel is currently open
-        require(
-            channel.channelStatus == ChannelStatus.Open,
-            "Channel is not open."
-        );
+  // Close an existing communication channel for a given escrow
+  function closeChannel(uint _escrowId) public {
+    // Fetch the corresponding channel
+    Channel storage channel = channels[_escrowId];
 
-        // Update channel status to Closed
-        channel.channelStatus = ChannelStatus.Closed;
-    }
+    // Ensure that the channel is currently open
+    require(
+      channel.channelStatus == ChannelStatus.Open,
+      "Channel is not open."
+    );
 
-    //*********************************************************************
-    //*********************************************************************
-    //                        Message Functions
-    //*********************************************************************
-    //*********************************************************************
+    // Update channel status to Closed
+    channel.channelStatus = ChannelStatus.Closed;
+  }
 
-    // Send a message in the channel (channelId == escrowId)
-    function sendMessage(
-        uint _escrowId,
-        string memory _content
-    ) public isEscrowParty(_escrowId) {
-        // Get channel by escrowId
-        Channel storage channel = channels[_escrowId];
+  //*********************************************************************
+  //*********************************************************************
+  //                        Message Functions
+  //*********************************************************************
+  //*********************************************************************
 
-        // Make sure only open Channels can be accessed
-        require(
-            channel.channelStatus == ChannelStatus.Open,
-            "Channel not open"
-        );
+  // Send a message in the channel (channelId == escrowId)
+  function sendMessage(uint _escrowId, string memory _content) public {
+    // Get channel by escrowId
+    Channel storage channel = channels[_escrowId];
 
-        // Determine the receiver based on the sender
-        address receiver = (msg.sender == channel.participantBuyer)
-            ? channel.participantSeller
-            : channel.participantBuyer;
+    // Make sure only open Channels can be accessed
+    require(channel.channelStatus == ChannelStatus.Open, "Channel not open");
 
-        // Create a new Message and store it in the mapping array
-        channelMessages[_escrowId][channel.messageCount] = Message({
-            sender: msg.sender,
-            receiver: receiver,
-            timestamp: block.timestamp,
-            content: _content
-        });
+    // Determine the receiver based on the sender
+    address receiver = (msg.sender == channel.participantBuyer)
+      ? channel.participantSeller
+      : channel.participantBuyer;
 
-        // Increase messageCount to ensure a unique ID for mapping
-        channel.messageCount++;
-    }
+    // Create a new Message and store it in the mapping array
+    channelMessages[_escrowId][channel.messageCount] = Message({
+      sender: msg.sender,
+      receiver: receiver,
+      timestamp: block.timestamp,
+      content: _content
+    });
 
-    //*********************************************************************
-    //*********************************************************************
-    //                        Modifier
-    //*********************************************************************
-    //*********************************************************************
+    // Increase messageCount to ensure a unique ID for mapping
+    channel.messageCount++;
+  }
 
-    // Modifier to check if the caller is one of the EscrowParty
-    modifier isEscrowEntity(uint256 _escrowId) {
-        (, , address _buyer, address _seller, , , , ) = escrowManager.getEscrow(
-            _escrowId
-        );
-        require(
-            msg.sender == _buyer || msg.sender == _seller,
-            "you are not party of this"
-        );
-        _;
-    }
+  //*********************************************************************
+  //*********************************************************************
+  //                        Modifier
+  //*********************************************************************
+  //*********************************************************************
+
+  // Modifier to check if the caller is one of the EscrowParty
+  modifier isEscrowEntity(uint256 _escrowId) {
+    (, , address _buyer, address _seller, , , , ) = escrowManager.getEscrow(
+      _escrowId
+    );
+    require(
+      msg.sender == _buyer || msg.sender == _seller,
+      "you are not party of this"
+    );
+    _;
+  }
 }
