0d31cfc9497b20b3b19b795deb6c7a68b0963b9b
ReviewApps admin <review-apps@saulo.dev>
2024-01-29 11:14:46 +0000

    [skip ci] - Updating branch ci

---
+++ ci/app/a75f78/assets/error-JV7huqUR.js
@@ -0,0 +1,6 @@
+import{c as n,d as c,j as r,D as s,f as e,w as t,as as _,o as i,u as d,B as l,L as u}from"./index-P5fyV6OH.js";/**
+ * @license lucide-vue-next v0.303.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const m=n("Undo2Icon",[["path",{d:"M9 14 4 9l5-5",key:"102s5s"}],["path",{d:"M4 9h10.5a5.5 5.5 0 0 1 5.5 5.5v0a5.5 5.5 0 0 1-5.5 5.5H11",key:"llx8ln"}]]),p={class:"flex items-center justify-center absolute inset-0"},f={class:"text-center"},h=s("div",{class:"text-4xl"}," Error ",-1),L=c({__name:"error",setup(x){return(k,v)=>{const o=u,a=_("RouterLink");return i(),r("div",p,[s("div",f,[h,e(a,{to:"/"},{default:t(()=>[e(o,{variant:"link"},{default:t(()=>[e(d(m),{size:16,class:"mr-2"}),l(" back to homepage ")]),_:1})]),_:1})])])}}});export{L as default};
