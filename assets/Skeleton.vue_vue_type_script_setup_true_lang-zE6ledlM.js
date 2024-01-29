7256e302b64073235205095face5a0f4a305d272
chubetho <chubetho@users.noreply.github.com>
2024-01-29 12:45:25 +0000

    Deploying to gh-pages from @ chubetho/skillchain@803fac66afef35def62f6466be00f768f5600c1a 🚀

--- assets/Skeleton.vue_vue_type_script_setup_true_lang-zE6ledlM.js
+++
@@ -1,6 +0,0 @@
-import{c as s,d as o,o as a,l as c,n as r,u as t,h as l}from"./index-NT2W6lgv.js";/**
- * @license lucide-vue-next v0.303.0 - ISC
- *
- * This source code is licensed under the ISC license.
- * See the LICENSE file in the root directory of this source tree.
- */const d=s("ChevronDownIcon",[["path",{d:"m6 9 6 6 6-6",key:"qrunsl"}]]),i=o({__name:"Skeleton",props:{class:{}},setup(e){const n=e;return(p,m)=>(a(),c("div",{class:r(t(l)("animate-pulse rounded-md bg-secondary",n.class))},null,2))}});export{d as C,i as _};
